44693720f83c7adee7ba4039a8fb17a6ecc02be3
Nathan Waterman <nathanjwtx@nathanjwtx.io>
2019-05-10 15:01:35 -0500

    Robot Name in progress

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -1,3 +1,13 @@
+import random
+import re
+
 class Robot(object):
     def __init__(self):
         pass
+    
+    def name(self):
+        alpha = (chr(random.randint(97, 123)) + chr(random.randint(97, 123))).upper()
+        digits = random.randint(0, 1000)
+        return alpha + str(digits)
+        
+
---
+++ robot-name/testing.py
@@ -0,0 +1,5 @@
+import random
+import robot_name
+from robot_name import make_robot
+
+robot_name
