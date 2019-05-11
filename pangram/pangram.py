9a81509a08eade9540bc124f7a06de5bbc2f866e
Nathan <nathanjwtx@nathanjwtx.io>
2019-05-11 12:50:16 -0500

    Completed Pangram

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,8 @@
 def is_pangram(sentence):
-    pass
+    if sentence == "":
+        return False
+    else:
+        for x in range(97, 123):
+            if chr(x) not in sentence.lower():
+                return False
+        return True
