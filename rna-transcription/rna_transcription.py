ed18289b88999f9ff37c5958a3d8ae19bc40d2da
Nathan <nathanjwtx@nathanjwtx.io>
2019-05-11 10:42:58 -0500

    RNA Transcription completed

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,2 +1,15 @@
 def to_rna(dna_strand):
-    pass
+    rna = {
+        "G": "C",
+        "C": "G",
+        "T": "A",
+        "A": "U"
+    }
+
+    if dna_strand == "":
+        return ""
+    else:
+        result = ""
+        for c in dna_strand:
+            result += rna[c]
+        return result
