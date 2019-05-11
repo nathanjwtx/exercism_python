a02084b6ad8b7a43413944c38d85b14b203fb0e1
Nathan <nathanjwtx@nathanjwtx.io>
2019-05-11 12:41:51 -0500

    Space Age completed

--- space-age/space_age.py
+++ space-age/space_age.py
@@ -1,3 +1,31 @@
 class SpaceAge(object):
+
+    earth = 365.25 * 24 * 60 * 60
+
     def __init__(self, seconds):
-        pass
+        self.seconds = seconds
+
+    def on_mercury(self):
+        return round(self.seconds / (0.2408467 * self.earth), 2)
+
+    def on_venus(self):
+        return round(self.seconds / (0.61519726 * self.earth), 2)
+
+    def on_mars(self):
+        return round(self.seconds / (1.8808158 * self.earth), 2)
+
+    def on_jupiter(self):
+        return round(self.seconds / (11.862615 * self.earth), 2)
+
+    def on_saturn(self):
+        return round(self.seconds / (29.447498 * self.earth), 2)
+
+    def on_uranus(self):
+        return round(self.seconds / (84.016846 * self.earth), 2)
+
+    def on_neptune(self):
+        return round(self.seconds / (164.79132 * self.earth), 2)
+
+    def on_earth(self):
+        return round(self.seconds / (1 * self.earth), 2)
+    
---
+++ space-age/testing.py
@@ -0,0 +1,8 @@
+import space_age
+
+age = space_age.SpaceAge(2134835688)
+
+print(age.on_mercury())
+
+age1 = space_age.SpaceAge(189839836)
+print(age.on_venus())
