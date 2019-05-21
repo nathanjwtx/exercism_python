87b9d5a976c6ebce2926d45e829cd87862ebfb71
Nathan Waterman <nathanjwtx@nathanjwtx.io>
2019-05-21 15:10:25 -0500

    Completed Hangman exercise

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -9,12 +9,34 @@ class Hangman(object):
     def __init__(self, word):
         self.remaining_guesses = 9
         self.status = STATUS_ONGOING
+        self.word = word
+        self.result = ["_"] * len(self.word)
 
     def guess(self, char):
-        pass
+        if self.remaining_guesses < 0:
+            self.status = STATUS_LOSE
+            raise ValueError("Guess limit reached")
+        elif self.status == STATUS_WIN:
+            raise ValueError("Game over man! It's game over!")
+        else:
+            blanks = self.result.count("_")
+            for x in range(0, len(self.word)):
+                if self.word[x] == char:
+                    self.result[x] = char
+            if blanks == self.result.count("_"):
+                self.remaining_guesses -= 1
 
     def get_masked_word(self):
-        pass
+        return "".join(self.result)
 
     def get_status(self):
-        pass
+        if self.result.count("_") > 1 and self.remaining_guesses < 0:
+            self.status = STATUS_LOSE
+        elif self.remaining_guesses >= 0 and self.word == "".join(self.result):
+            self.status = STATUS_WIN
+        else:
+            self.status = STATUS_ONGOING
+        return self.status
+
+    def get_guesses(self):
+        return self.remaining_guesses
