299db8ae1cdac4c3821bc92163b1f135b9faaa02
Nathan Waterman <nathanjwtx@nathanjwtx.io>
2019-05-21 09:06:49 -0500

    Completed Darts

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,13 @@
 def score(x, y):
-    pass
+    def test(x, y): 
+        return x**2 + y**2
+
+    if test(x, y) > 100:
+        return 0
+    elif test(x, y) > 25:
+        return 1
+    elif test(x, y) > 1:
+        return 5
+    else:
+        return 10
+
