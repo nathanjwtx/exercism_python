06cc9abe92d18530ab1a49f9a119710e4328f3b3
Nathan <nathanjwtx@nathanjwtx.io>
2019-05-22 12:49:25 -0500

    Completed High Scores and Protein Translation challenges

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -1,10 +1,12 @@
 def latest(scores):
-    pass
+    return scores.pop()
 
 
 def personal_best(scores):
-    pass
+    scores.sort(reverse=True)
+    return scores[0]
 
 
 def personal_top_three(scores):
-    pass
+    scores.sort(reverse=True)
+    return scores[0:3] if len(scores) >= 3 else scores[0: len(scores)]
--- protein-translation/protein_translation.py
+++ protein-translation/protein_translation.py
@@ -1,2 +1,32 @@
 def proteins(strand):
-    pass
+    codon = {
+        "AUG": "Methionine",
+        "UUU": "Phenylalanine",
+        "UUC": "Phenylalanine",
+        "UUA": "Leucine",
+        "UUG": "Leucine",
+        "UCU": "Serine",
+        "UCC": "Serine",
+        "UCA": "Serine",
+        "UCG": "Serine",
+        "UAU": "Tyrosine",
+        "UAC": "Tyrosine",
+        "UGU": "Cysteine",
+        "UGC": "Cysteine",
+        "UGG": "Tryptophan",
+        "UAA": "STOP",
+        "UAG": "STOP",
+        "UGA": "STOP"
+    }
+
+    result = []
+    x = 0
+
+    while True:
+        if x*3+1 <= len(strand) and strand[x*3:x*3+3] not in ["UAA", "UAG", "UGA", ""]:
+            result.append(codon[strand[x*3:x*3+3]])
+            x += 1
+        else:
+            break
+    
+    return result
